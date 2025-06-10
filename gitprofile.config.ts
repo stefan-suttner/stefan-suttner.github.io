732a77d54b5f4138915e04c2bd44f745fd2badd3
stefan-suttner <sers+github@mailbox.org>
2025-06-10 15:18:13 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -83,20 +83,10 @@ const CONFIG = {
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'International Trade',
+    'Trade Policy',
+    'Political Economy',
+    'International Institutions',
   ],
   experiences: [
     {
